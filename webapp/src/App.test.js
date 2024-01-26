e0807efc3d0e588452d89b21109eb7831f5ccabd
Jose Emilio Labra Gayo <jelabra@gmail.com>
2024-01-26 11:03:54 +0100

    Changed wiq_0 by wiq_en2a

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to wiq_0/i);
+  const linkElement = screen.getByText(/Welcome to wiq_en2a/i);
   expect(linkElement).toBeInTheDocument();
 });
