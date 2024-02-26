98844b1543b132971ae73d2a58bee73adb41aa02
Miguel <UO277566@uniovi.es>
2024-02-26 19:21:02 +0100

    Group Entity Fields

--- game/groupservice/group-model.js
+++ game/groupservice/group-model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 // TODO: HACER AQUI LA ENTIDAD GRUPO
 
 const groupSchema = new mongoose.Schema({
-    // ...
+    groupName: String,
+    numberOfMembers: int
 });
 
 const Group = mongoose.model('Group', groupSchema);
