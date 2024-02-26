aeb2e30b9c222ff17286e31e651636a597250d43
Miguel <UO277566@uniovi.es>
2024-02-26 20:52:18 +0100

    Definir endpoints

--- game/groupservice/group-service.js
+++ game/groupservice/group-service.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Group = require('./group-model')
+const Group = require('./group-model');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8004; 
 
 
+app.use(bodyParser.json());
+
 // Connect to MongoDB 
 // const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
 // mongoose.connect(mongoUri);
+
+// Function to validate required fields in the request body
+function validateRequiredFields(req, requiredFields) {
+  for (const field of requiredFields) {
+    if (!(field in req.body)) {
+      throw new Error(`Missing required field: ${field}`);
+    }
+  }
+}
+
 
 
 // TODO: hacer peticiones (get/post/put ...) para manejar logica de grupos
@@ -16,9 +29,23 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to group service module' });
 });
 
+app.post('/join', (req,requiredFields) => {
+  res.json({ message: 'Joining Group' });
+  //
+});
+
+app.post('/leave', (req,requiredFields) => {
+  res.json({ message: 'Leaving Group' });
+});
+
+app.post('/create', (req,requiredFields) =>{
+  res.json({ message: 'Creating Group' });
+});
+
+app.put('/')
 
 const server = app.listen(port, () => {
-  console.log(`Auth Service listening at http://localhost:${port}`);
+  console.log(`Group Service listening at http://localhost:${port}`);
 });
 
 server.on('close', () => {
