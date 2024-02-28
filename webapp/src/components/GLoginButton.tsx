29dd0227990da81f535ace169278c1186b10ed30
uo287741 <uo287741@uniovi.es>
2024-02-28 10:47:30 +0100

    Google Login V2
    Email extraction from user token

--- webapp/src/components/GLoginButton.tsx
+++ webapp/src/components/GLoginButton.tsx
@@ -9,12 +9,28 @@ const GLoginButton = () => {
     const onSuccess = (response: any) => {
         console.log(  "LOGIN SUCCESS! Current User: ",response.profileObject); 
          // handle successful login""
+         var authResponse = response.getAuthResponse();
+         var accessToken = authResponse.access_token;
+
+         //Validations in backend should be done
+        
+         // Obtener el perfil del usuario
+          var profile = accessToken.getBasicProfile();
+    
+         // Obtener el email del usuario
+         var email = profile.getEmail();
+         console.log(email);
+         //send email to backend
+         
     };
 
     const onFailure = (error: any) => {
         console.log(  "LOGIN FAILED! Error: ",error); 
         // handle failed login
     };
+
+
+    
 
     return (
         <div id='signInButton'>
@@ -24,8 +40,10 @@ const GLoginButton = () => {
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
             />
         </div>
+        
     )
 }
 
