9887776077e9a6a9794323015d02e03293ab6db7
carlospelazas <carlospelazas@gmail.com>
2024-03-07 10:05:48 +0100

    get lastGame by username

--- game/gameservice/GameController.js
+++ game/gameservice/GameController.js
@@ -1,12 +1,17 @@
 let Game = require('./game-model');
-let User = require('../../users/userservice/user-model');
+let { userSchema } = require('../../users/userservice/user-model');
+const {question4AnswersSchema} = require("../qgservice/Question4Answers");
 const { createGame } = require('./gameLogic');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('User', userSchema);
+const Question4Answers = mongoose.model('Question4Answers', question4AnswersSchema);
 
 let GameController = {
     findByUsername: async (req, res) => {
-        let game = await User.find({user: req.params.username}).populate("lastgame");
-        res.json(game);
+        let game = await User.find({username: req.params.username}).populate("lastGame")
+        let response = await Game.findById(game[0].lastGame.id).populate("questions").populate("players")
+        res.json(response);
     },
     create: async (req, res) => {
         const { questions, users } = req.body;
