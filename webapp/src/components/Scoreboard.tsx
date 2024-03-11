18e64f07623cc1235fcc60e6f07aaba0fbeeda36
uo287741 <uo287741@uniovi.es>
2024-03-11 17:21:12 +0100

    Scoreboard added to the game page

--- webapp/src/components/Scoreboard.tsx
+++ webapp/src/components/Scoreboard.tsx
@@ -1,14 +1,28 @@
 
 
 
-const Scoreboard = (
-    
-) => {
+import { useState } from 'react';
+import { Score } from './Game';
 
+const [sorted, setSorted] = useState<Score[]>([]);
+
+const Scoreboard = (userScores: Score[]) => {
+
+    //Get the scores and sort them
+   setSorted( userScores.sort((a, b) => { 
+        return parseInt(b.points) - parseInt(a.points);
+    }));
+
+    //Return the scoreboard
     return (
         <div>
             <h1>Scoreboard</h1>
             <p>Here is the scoreboard</p>
+            <ul>
+                {sorted.map((score, index) => {
+                    return <li key={index}>{score.username}: {score.points}</li>
+                })} 
+            </ul>
         </div>
     )
 }
