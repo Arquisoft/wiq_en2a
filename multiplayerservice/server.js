e5fa9286616702c08c0d71a2bf75183e081b1db6
carlospelazas <carlospelazas@gmail.com>
2024-03-25 17:43:07 +0100

    [WIP] lobby creation

--- multiplayerservice/server.js
+++ multiplayerservice/server.js
@@ -1,25 +1,70 @@
 const express = require('express');
-const axios = require('axios');
-const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
-const port = 8006;
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// app.use(bodyParser.json());
-app.use(express.json());
+const parties = {}; // Store active parties and their corresponding sockets
 
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
-mongoose.connect(mongoUri);
+// Generate a random code for the party
+function generatePartyCode() {
+  return Math.random().toString(36).substr(2, 6).toUpperCase();
+}
 
-app.get('/', (req, res) => {
-  res.json({
-    "hi": "multiplayer service"
+// Create a new party
+function createParty() {
+  const partyCode = generatePartyCode();
+  parties[partyCode] = [];
+  return partyCode;
+}
+
+// Join a party
+function joinParty(partyCode, socket) {
+  if (parties[partyCode]) {
+    parties[partyCode].push(socket);
+    return true;
+  }
+  return false;
+}
+
+io.on('connection', socket => {
+  console.log('Client connected');
+
+  // Create a new party
+  socket.on('createParty', () => {
+    const partyCode = createParty();
+    socket.emit('partyCreated', partyCode);
+  });
+
+  // Join an existing party
+  socket.on('joinParty', partyCode => {
+    if (joinParty(partyCode, socket)) {
+      socket.join(partyCode);
+      socket.emit('joinedParty', partyCode);
+      console.log(`User joined party: ${partyCode}`);
+    } else {
+      socket.emit('partyNotFound');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    
+    // Remove the socket from all parties
+    for (const partyCode in parties) {
+      const index = parties[partyCode].indexOf(socket);
+      if (index !== -1) {
+        parties[partyCode].splice(index, 1);
+        console.log(`User left party: ${partyCode}`);
+        break; // Assuming user can only be in one party at a time
+      }
+    }
   });
 });
 
-
-const server = app.listen(port, () => {
-  console.log(`Multiplayer Service listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 8006;
+server.listen(PORT, () => {
+  console.log(`Multiplayer Service listening at http://localhost:${PORT}`);
 });
-
-module.exports = server;
