c2e228157add103ef27bc6ef6f8d34d9f68e7c44
CarolinaUniovi <uo275673@uniovi.es>
2024-04-08 17:40:17 +0200

    starting the members group table

--- webapp/src/pages/groups/index.tsx
+++ webapp/src/pages/groups/index.tsx
@@ -8,24 +8,38 @@ const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000
 
 export const GroupsPage: React.FC<{}> = () => {
   const [signedUp, setSignedUp] = useState(true);
-  const uuid =  JSON.stringify(localStorage.getItem("userUUID")).replace("\"", "").replace("\"", "");
+  const userUuid =  JSON.stringify(localStorage.getItem("userUUID")).replace("\"", "").replace("\"", "");
+  let alreadyDone = false;
 
-  const isSignedUp = async () =>{
-    try{
-      const groups = await axios.get(`${apiEndpoint}/getGroup/`+uuid);
-      console.log(groups);
-      setSignedUp(true);
-    }catch (error:any) {
-      setSignedUp(false);
+  const isSignedUp = async (recheck: boolean) =>{
+    if(!alreadyDone || recheck){
+      try{
+        const groups = await axios.get(`${apiEndpoint}/getGroups`);
+        for(var group of groups.data){
+          for(var member of group.members){
+            const uuid = JSON.stringify(member).replace("\"", "").replace("\"", "");
+            if(userUuid == uuid){
+              setSignedUp(true);
+            } 
+          }
+        }
+        alreadyDone = true;
+        setSignedUp(false);
+      }catch (error:any) {
+        setSignedUp(false);
+      }
     }
-  }
+    
+  };
 
-  isSignedUp()
+  isSignedUp(false);
+  
   return(
     <Container sx={{ mt: 9 }} maxWidth="xl" className="groups-container">
       {
-        signedUp && (
-        <NoGroup/>)
+        signedUp? (
+          <NoGroup/>
+        ): (<div></div>)
       };
     </Container>
   );
