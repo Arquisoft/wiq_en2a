1a71a8112c4185d3daa557c9cff335c28a3fc547
AlvaroIC <alvaroibanez02@gmail.com>
2024-04-09 03:29:28 +0200

    White color for font in profile

--- webapp/src/pages/userProfile/index.tsx
+++ webapp/src/pages/userProfile/index.tsx
@@ -24,8 +24,8 @@ const ProfilePage = () => {
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <Paper elevation={3} sx={{ p: 3, backgroundColor: '#1976d2' }}>
-              <Typography variant="h4" gutterBottom className='profile-subheader'>Personal Information</Typography>
-              <ul>
+              <Typography color="#ffffff" variant="h4" gutterBottom className='profile-subheader'>Personal Information</Typography>
+              <ul className="white-list">
                 <li><Typography variant="body1" className='field'>{t('profile_name')} { user }</Typography></li>
                 <li><Typography variant="body1" className='field'>{t('profile_email')} { email }</Typography></li>
                 <li><Typography variant="body1" className='field'>{t('profile_created_at')} {formatDate(createdAt) }</Typography></li>
@@ -34,8 +34,8 @@ const ProfilePage = () => {
           </Grid>
           <Grid item xs={12} md={6}>
             <Paper elevation={3} sx={{ p: 3, backgroundColor: '#1976d2' }}>
-              <Typography variant="h4" gutterBottom className='profile-subheader'>Performance Statistics</Typography>
-              <ul>
+              <Typography color="#ffffff" variant="h4" gutterBottom className='profile-subheader'>Performance Statistics</Typography>
+              <ul className="white-list">
                 <li><Typography variant="body1" className='field'>{t('profile_points')} { JSON.stringify(Number(score)) }</Typography></li>
                 <li><Typography variant="body1" className='field'>{t('profile_nwins')} { JSON.stringify(Number(nwins)) }</Typography></li>
                 <li><Typography variant="body1" className='field'>{t('profile_n_correct_answers')} { JSON.stringify(Number(nCorrectAnswers)) }</Typography></li>
@@ -45,8 +45,7 @@ const ProfilePage = () => {
           </Grid>
           <Grid item xs={12} md={6}>
             <Paper elevation={3} sx={{ p: 3, backgroundColor: '#1976d2' }}>
-              <Typography variant="h4" gutterBottom className='profile-subheader'>Additional Information</Typography>
-              {/* Add additional information here */}
+              <Typography color="#ffffff" variant="h4" gutterBottom className='profile-subheader'>Additional Information</Typography>
             </Paper>
           </Grid>
         </Grid>
