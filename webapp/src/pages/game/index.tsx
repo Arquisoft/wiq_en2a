c45dd2b7c3db4b7218d6f3b0743f64d443ba6a9b
AlvaroIC <alvaroibanez02@gmail.com>
2024-04-14 01:14:33 +0200

    Game page css to scss

--- webapp/src/pages/game/index.tsx
+++ webapp/src/pages/game/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./GamePage.css";
+import "./game-page.scss";
 
 export const GamePage: React.FC<{}> = () => {
   return (
