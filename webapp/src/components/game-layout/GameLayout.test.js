5155932888225fcb4f5d9b2cec8dc0bf3afeb0f9
uo287741 <uo287741@uniovi.es>
2024-04-22 19:17:48 +0200

    First Version of GameLayout Tests, needs improvement for the second and third

---
+++ webapp/src/components/game-layout/GameLayout.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import GameLayout from './GameLayout';
+import { MemoryRouter } from 'react-router-dom'; // Importa MemoryRouter
+
+describe('GameLayout component', () => {
+  it('renders Game by default', () => {
+    render(
+        <MemoryRouter>
+            <GameLayout />
+        </MemoryRouter>
+);
+    expect(screen.getByTestId('game-header')).toBeInTheDocument();
+    expect(screen.getByTestId('game-link')).toBeInTheDocument();
+    expect(screen.queryByTestId('groups-page-component')).toBeNull();
+    expect(screen.queryByTestId('scoreboard-component')).toBeNull();
+  });
+
+  it('renders GroupsPage when Groups link is clicked', () => {
+    render(
+        <MemoryRouter>
+            <GameLayout />
+        </MemoryRouter>
+);
+    fireEvent.click(screen.getByTestId('groups-link'));
+    expect(screen.queryByTestId('game-component')).toBeNull();
+    expect(screen.getByTestId('groups-page-component')).toBeInTheDocument();
+    expect(screen.queryByTestId('scoreboard-component')).toBeNull();
+  });
+
+  it('renders Scoreboard when Scoreboard link is clicked', () => {
+    render(<GameLayout />);
+    fireEvent.click(screen.getByTestId('scoreboard-link'));
+    expect(screen.queryByTestId('game-component')).toBeNull();
+    expect(screen.queryByTestId('groups-page-component')).toBeNull();
+    expect(screen.getByTestId('scoreboard-component')).toBeInTheDocument();
+  });
+});
