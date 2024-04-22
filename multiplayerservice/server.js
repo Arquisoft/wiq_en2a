f9915b2421773194a88b265ee57df15d36b1cd3b
carlospelazas <carlospelazas@gmail.com>
2024-04-22 20:59:30 +0200

    small fix

--- multiplayerservice/server.js
+++ multiplayerservice/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: 'http://conoceryvencer.xyz',
-    origin: 'http://localhost',
+    //origin: 'http://localhost',
     methods: ['GET', 'POST'],
     credentials: true
   }
