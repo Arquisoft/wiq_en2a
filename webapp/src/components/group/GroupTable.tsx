6e346afa59c3cd02c00c324f6cd9f7ca9b38a241
AlvaroIC <alvaroibanez02@gmail.com>
2024-04-23 19:44:03 +0200

    Group table localized

--- webapp/src/components/group/GroupTable.tsx
+++ webapp/src/components/group/GroupTable.tsx
@@ -1,6 +1,7 @@
 import  {  useEffect } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Container, Grid, Button } from "@mui/material"
 import axios from 'axios';
+import { useTranslation } from 'react-i18next';
 
 type TableProps = {
     groupUUID: string,
@@ -25,6 +26,9 @@ const apiEndpoint = 'http://localhost:8000'
 //const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 
 export const GroupTable = (props: TableProps) => {
+
+    const { t } = useTranslation();
+
     const aFunction = async ()=>{
         await axios.get(`${apiEndpoint}/getGroup/`+props.groupUUID).then(res => {
             console.log(res.data);
@@ -78,13 +82,13 @@ export const GroupTable = (props: TableProps) => {
                         <h1 style={{margin:'1em'}} >{groupName}</h1>
                     </Grid>
                     <Grid item xs={3} >
-                        <h1 style={{margin:'1em'}} >{total} points</h1>
+                        <h1 style={{margin:'1em'}} >{total}{t('group_table_points')}</h1>
                     </Grid>
                     <Grid item xs={3} >
-                        <h1 style={{margin:'1em'}} >{numberMembers} members</h1>
+                        <h1 style={{margin:'1em'}} >{numberMembers}{t('group_table_members')}</h1>
                     </Grid>
                     <Grid item xs={3} >
-                        <Button style={{maxWidth: '250px', maxHeight: '50px', minWidth: '250px', minHeight: '50px', float: 'right', margin:'1em'}} variant="contained" onClick={leaveGroup} >Leave</Button>
+                        <Button style={{maxWidth: '250px', maxHeight: '50px', minWidth: '250px', minHeight: '50px', float: 'right', margin:'1em'}} variant="contained" onClick={leaveGroup} >{t('group_table_leave')}</Button>
                     </Grid>
                 </Grid>
             )}
@@ -92,9 +96,9 @@ export const GroupTable = (props: TableProps) => {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Username</TableCell>
-                            <TableCell>Role</TableCell>
-                            <TableCell>Score</TableCell>
+                            <TableCell>{t('group_table_username')}</TableCell>
+                            <TableCell>{t('group_table_role')}</TableCell>
+                            <TableCell>{t('group_table_score')}</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
