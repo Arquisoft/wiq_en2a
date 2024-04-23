1b52ad0cd26af9f0487cb40613bb5bd04dbe1c01
AlvaroIC <alvaroibanez02@gmail.com>
2024-04-23 20:45:02 +0200

    Game page localization

--- webapp/src/pages/game/index.tsx
+++ webapp/src/pages/game/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./game-page.scss";
+import { useTranslation } from 'react-i18next';
 
 export const GamePage: React.FC<{}> = () => {
+
+  const { t } = useTranslation();
+
   return (
     <div className="game-page-container">
       <button className="game-page-button">
-        <Link to="/game/single-player">Single Player</Link>
+        <Link to="/game/single-player">{t('game_single_player')}</Link>
       </button>
       <button className="game-page-button">
-        <Link to="/game/single-player-ai">Single Player (AI)</Link>
+        <Link to="/game/single-player-ai">{t('game_single_player_ai')}</Link>
       </button>
       <button className="game-page-button">
-        <Link to="/game/multi-player">Multiplayer</Link>
+        <Link to="/game/multi-player">{t('game_multiplayer')}</Link>
       </button>
     </div>
   );
