9922b34304b24f825e796e4a56f6576feba27f57
uo287741 <uo287741@uniovi.es>
2024-04-25 20:33:19 +0200

    component test for lobby and addition in single player

--- webapp/src/components/game/singleplayer/LobbyGameSinglePlayer.tsx
+++ webapp/src/components/game/singleplayer/LobbyGameSinglePlayer.tsx
@@ -40,27 +40,31 @@ const LobbyGame: FC<LobbyGameProps> = ({setPlayers, players, setCurrentStage, is
       <h2 className='lobby-title'>Lobby - Single player</h2>
         <div>
           {players.map((player, index) => (
-            <div key={player.username} className='player-item'>
+            <div key={player.username} className='player-item' data-testid="player-item">
               <img src={"https://robohash.org/"+player.username+".png"} alt={player.username} />
               <p>{player.username}</p>
               <p>Total points: {player.points}</p>
               {!player.isBot && (
-                <button className="delete-button" disabled>Delete</button>
+                <button data-testid="delete-button" className="delete-button" disabled>Delete</button>
               )}
               {player.isBot && (
-                <button onClick={() => deletePlayer(index)} className="delete-button">Delete</button>
+                <button onClick={() => deletePlayer(index)} data-testid="delete-button"
+                 className="delete-button">Delete</button>
               )}
             </div>
           ))}
         </div>
       <div className='button-container'>
-        <button disabled={players.length === 4} onClick={addBotPlayer} className="add-bot-button">
+        <button disabled={players.length === 4} onClick={addBotPlayer} className="add-bot-button"
+         data-testid="add-bot-button">
           Add Bot Player
         </button>
-        {isFetched && <button className="start-game-button" onClick={() => setCurrentStage(2)}>
+        {isFetched && <button data-testid="start-game-button"
+         className="start-game-button" onClick={() => setCurrentStage(2)}>
             Start Game
         </button>}
-        {!isFetched && <button className="start-game-button" onClick={() => setCurrentStage(2)} disabled>
+        {!isFetched && <button data-testid="start-game-button"
+         className="start-game-button" onClick={() => setCurrentStage(2)} disabled>
             Loading questions...
         </button>}
       </div>
