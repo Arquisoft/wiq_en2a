5f3b9d44f1d6736e47ba0a77dea88a7ba57aa87b
carlospelazas <carlospelazas@gmail.com>
2024-04-25 11:59:39 +0200

    get user by username

--- users/userservice/user-service.test.js
+++ users/userservice/user-service.test.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const User = require('./user-model');
-const isValidUuidV4 = require('./util/ValidateUUID')
 
 let mongoServer;
 let app;
@@ -11,18 +10,19 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
   process.env.MONGODB_URI = mongoUri;
+
   mockUser = new User({
     uuid: '3c68688e-84e7-4d29-b7c7-09474d42b669', // Valid v4 UUID
-    username: 'XXXXXXXX',
-    password: 'XXXXXXXX',
+    username: 'mockUser',
+    password: 'testpassword',
     createdAt: '2022-01-01T00:00:00.000Z',
     nCorrectAnswers: 10,
     nWrongAnswers: 5,
     totalScore: 15,
     nWins: 2,
   });
-  jest.spyOn(User, 'findOne').mockResolvedValue(mockUser)
-  app = require('./user-service'); 
+
+  app = require('./user-service');
 });
 
 afterEach(async () => {
@@ -30,9 +30,10 @@ afterEach(async () => {
 })
 
 afterAll(async () => {
-    app.close();
-    await mongoServer.stop();
+  app.close()
+  await mongoServer.stop();
 });
+
 
 describe('User Service', () => {
   it('should add a new user on POST /adduser', async () => {
@@ -103,26 +104,23 @@ describe('User Service', () => {
   })
 
 it('updates last game for valid players', async () => {
-  const gameUUID = '123e4567-e89b-12d3-a456-426655440001'; // Valid v4 UUID
+  const gameUUID = '123e4567-e89b-12d3-a456-426655440001';
   const players = [
     {
-      uuid: '3c68688e-84e7-4d29-b7c7-09474d42b669', // Valid v4 UUID
-    },
-    {
-      uuid: '3c68688e-84e7-4d29-b7c7-09474d42b669', // Valid UUID with extra spaces (trimmed)
+      uuid: '3c68688e-84e7-4d29-b7c7-09474d42b669',
     },
   ];
-
   mockUser.lastGameId = undefined; // Reset lastGameId for testing
+  jest.spyOn(User, 'findOne').mockResolvedValueOnce(mockUser);
 
   const response = await request(app).post('/updateLastGame').send({ gameUUID, players });
 
   expect(response.statusCode).toBe(200);
-  expect(response.body.message).toEqual('Last game updated for 2 users.');
+  expect(response.body.message).toEqual('Last game updated for 1 users.');
 
   // Assertions on User model interactions (mock calls)
 
-  expect(User.findOne).toHaveBeenCalledTimes(2); // User.findOne called twice (once per player)
+  expect(User.findOne).toHaveBeenCalledTimes(1); // User.findOne called twice (once per player)
   expect(mockUser.lastGameId).toEqual(gameUUID); // Updated lastGameId on mock user
 });
 
@@ -157,6 +155,21 @@ it('returns informative error for user not found', async () => {
 
   expect(response.statusCode).toBe(500); // Simulate internal server error
   expect(response.body.error).toBeDefined(); // Expect an error message
+});
+
+it('retrieves user by username and returns user data without password field', async () => {
+  const mockUserWithoutPassword = { ...mockUser };
+  delete mockUserWithoutPassword.password;
+
+  jest.spyOn(User, 'findOne').mockImplementation((filter) => ({
+    select: jest.fn().mockResolvedValueOnce(mockUserWithoutPassword)
+  }));
+  
+  // Make request to the endpoint
+  const response = await request(app)
+    .get(`/getUser/${mockUser.username}`);
+
+  expect(response.status).toBe(200);
 });
 
 });
