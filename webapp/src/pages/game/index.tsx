00ce85be2d5be4070ad1e8b8e910c062114610d6
AlvaroIC <alvaroibanez02@gmail.com>
2024-04-26 18:25:38 +0200

    Deleted single player (AI) button

--- webapp/src/pages/game/index.tsx
+++ webapp/src/pages/game/index.tsx
@@ -13,9 +13,6 @@ export const GamePage: React.FC<{}> = () => {
         <Link to="/game/single-player">{t('game_single_player')}</Link>
       </button>
       <button className="game-page-button">
-        <Link to="/game/single-player-ai">{t('game_single_player_ai')}</Link>
-      </button>
-      <button className="game-page-button">
         <Link to="/game/multi-player">{t('game_multiplayer')}</Link>
       </button>
     </div>
