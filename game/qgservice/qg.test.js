91f1962c80474281d4176eb0e11c4a9b4663e0e5
carlospelazas <carlospelazas@gmail.com>
2024-05-06 12:42:23 +0200

    math questions

--- game/qgservice/qg.test.js
+++ game/qgservice/qg.test.js
@@ -7,6 +7,7 @@ const { executeSparqlQuery } = require('./generatorLogic/SparqlQuery')
 const { bindCapitalsResults } = require('./generatorLogic/BindResults')
 const { createMathQuestions, generateRandomMathQuestion } = require('./generatorLogic/MathQuestions')
 const axios = require('axios');
+const uuid = require('uuid');
 
 let app;
 let mongoServer;
@@ -176,6 +177,58 @@ it('should handle empty query result', () => {
   // Assertions
   expect(capitalsMap).toBeInstanceOf(Map);
   expect(capitalsMap.size).toBe(0);
+});
+
+it('should generate a math question with valid operands and operator', () => {
+
+  const mathQuestion = generateRandomMathQuestion();
+
+  expect(mathQuestion).toHaveProperty('uuid');
+  expect(mathQuestion).toHaveProperty('question');
+  expect(mathQuestion).toHaveProperty('correctAnswer');
+  expect(mathQuestion).toHaveProperty('incorrectAnswer1'); 
+  expect(mathQuestion).toHaveProperty('incorrectAnswer2');
+  expect(mathQuestion).toHaveProperty('incorrectAnswer3');
+
+  const [operand1, operator, operand2] = mathQuestion.question.split(' ');
+
+  expect(parseInt(operand1)).toBeGreaterThanOrEqual(1);
+  expect(parseInt(operand1)).toBeLessThanOrEqual(100);
+  expect(parseInt(operand2)).toBeGreaterThanOrEqual(1);
+  expect(parseInt(operand2)).toBeLessThanOrEqual(100);
+
+  expect(['+', '-', '*', '/']).toContain(operator);
+
+  const correctAnswer = eval(mathQuestion.question);
+  expect(mathQuestion.correctAnswer).toEqual(correctAnswer.toString());
+
+  expect(mathQuestion.incorrectAnswer1).not.toEqual(correctAnswer.toString());
+  expect(mathQuestion.incorrectAnswer2).not.toEqual(correctAnswer.toString());
+  expect(mathQuestion.incorrectAnswer3).not.toEqual(correctAnswer.toString());
+});
+
+it('should create math questions and return them', async () => {
+  const numberOfQuestions = 3;
+
+  const result = await createMathQuestions(numberOfQuestions);
+
+  expect(Array.isArray(result)).toBe(true);
+  expect(result.length).toBe(3);
+
+  result.forEach((question) => {
+    expect(question).toHaveProperty('correctAnswer');
+    expect(question).toHaveProperty('incorrectAnswer1');
+    expect(question).toHaveProperty('incorrectAnswer2');
+    expect(question).toHaveProperty('incorrectAnswer3');
+    expect(question).toHaveProperty('question');
+    expect(question).toHaveProperty('uuid');
+    expect(typeof question.correctAnswer).toBe('string');
+    expect(typeof question.incorrectAnswer1).toBe('string');
+    expect(typeof question.incorrectAnswer2).toBe('string');
+    expect(typeof question.incorrectAnswer3).toBe('string');
+    expect(typeof question.question).toBe('string');
+    expect(typeof question.uuid).toBe('string');
+  });
 });
 
 })
